171001148925b4292fc915f612989f1e379dca26
JansenSmith <jansen@bstarengineering.com>
2024-03-13 11:06:15 -0400

    new CAD loader script

---
+++ joystickCovers.groovy
@@ -0,0 +1,32 @@
+import eu.mihosoft.vrl.v3d.parametrics.*;
+import java.util.stream.Collectors;
+import com.neuronrobotics.bowlerstudio.vitamins.Vitamins;
+import eu.mihosoft.vrl.v3d.CSG;
+import eu.mihosoft.vrl.v3d.Cube;
+CSG generate(){
+	String type= "joystickCovers"
+	if(args==null)
+		args=["nintendoSwitch"]
+	// The variable that stores the current size of this vitamin
+	StringParameter size = new StringParameter(	type+" Default",args.get(0),Vitamins.listVitaminSizes(type))
+	HashMap<String,Object> measurments = Vitamins.getConfiguration( type,size.getStrValue())
+
+	def heightValue = measurments.height
+	def innerRadiusValue = measurments.innerRadius
+	def massCentroidXValue = measurments.massCentroidX
+	def massCentroidYValue = measurments.massCentroidY
+	def massCentroidZValue = measurments.massCentroidZ
+	def massKgValue = measurments.massKg
+	def outerRadiusValue = measurments.outerRadius
+	def priceValue = measurments.price
+	def sourceValue = measurments.source
+	for(String key:measurments.keySet().stream().sorted().collect(Collectors.toList())){
+		println "joystickCovers value "+key+" "+measurments.get(key);
+}
+	// Stub of a CAD object
+	CSG part = new Cube().toCSG()
+	return part
+		.setParameter(size)
+		.setRegenerate({generate()})
+}
+return generate() 
